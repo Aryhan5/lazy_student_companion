fc13ea11c50c4446be88b2a83c6d3a7e5b49b84e
Aryhan5 <serena.sadek@outlook.com>
2023-08-15 21:33:54 +0200

    audio segment (timeframe based)

--- audioprocess.py
+++ audioprocess.py
@@ -1,26 +1,35 @@
 import whisper
+import math
 from pydub import AudioSegment
+import scipy.io.wavfile as wav
 
 # Python code to convert video to audio
 import moviepy.editor as mp
 
-def extract_audio(video_file):
+def extract_audio(video_file, output_path="./source/test.wav"):
     clip = mp.VideoFileClip(video_file)
-    clip.audio.write_audiofile("./test.wav",codec='pcm_s16le')
+    clip.audio.write_audiofile(output_path,codec='pcm_s16le')
 
 def audio_transcript(path, prompt=""):
     model = whisper.load_model("base")
     result = model.transcribe(path)
     return(result["text"])
 
-def audio_segment(path, lengh=3): #lengh in minutes
+def audio_segment(path, lengh=5, timestamp=None, output_path="source/segment.wav"): #lengh in minutes
     song = AudioSegment.from_file(path)
+    (source_rate, source_sig) = wav.read(path)
+    duration_seconds = len(source_sig) / float(source_rate)
 
-    # PyDub handles time in milliseconds
-    newAudio = AudioSegment.from_wav(path)
-    newAudio = newAudio[:(lengh*1000*60)]
-    newAudio.export('segment.wav', format="wav") #Exports to a wav file in the current path.
+    nfrag = int(math.ceil(duration_seconds/60)/lengh)
 
-short = audio_segment("test.wav", 5)
-transcript = audio_transcript("segment.wav")
-print(transcript)
+    for i in range (nfrag):
+
+        # PyDub handles time in milliseconds
+        newAudio = AudioSegment.from_wav(path)
+        newAudio = newAudio[((i)*1000*60*lengh):((i+1)*1000*60*lengh)]
+        newAudio.export("source/segment"+str(i)+".wav", format="wav") #Exports to a wav file in the current path.
+
+
+audio_segment("./source/test.wav")
+'''transcript = audio_transcript("./source/segment.wav")
+print(transcript)'''
